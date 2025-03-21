96599eb9f3cd56dd0122d61173979edd71f03dd0
Barbazzo Fernap <196495312+Barbazzo-Fernap@users.noreply.github.com>
2025-03-21 14:04:13 +0000

    remove tracker

--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-P1QT24YPSK"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-P1QT24YPSK');
-      </script>
-
       <header>
 
       </header>
